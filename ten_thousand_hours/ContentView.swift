c0ce095f4bf3f599f6302b4fd51ca10d2f964147
Yiwei Yu <yuyiwei@berkeley.edu>
2023-11-16 12:17:41 -0800

    Initial Commit

---
+++ ten_thousand_hours/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ten_thousand_hours
+//
+//  Created by 余懿炜 on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ten_thousand_hours/ten_thousand_hoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  ten_thousand_hoursApp.swift
+//  ten_thousand_hours
+//
+//  Created by 余懿炜 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ten_thousand_hoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
