845f75bcf1756b4fc3d4e16635ebb82237bb9514
Yiwei Yu <yuyiwei@berkeley.edu>
2023-12-30 23:37:15 +0800

    拆分Login and Signup using MVVM Model“

--- frontend/ten_thousand_hours/Views/LoginView.swift
+++ frontend/ten_thousand_hours/Views/LoginView.swift
@@ -8,94 +8,65 @@ import SwiftUI
 import RealmSwift
 
 struct LoginView: View {
+    @StateObject var viewModel = LoginViewModel()
     @Binding var username: String
     @Binding var level: String
     @Environment(\.dismiss) var dismiss
-    @State private var isLoggingIn = false
-    @State private var showingSignup = false
-    @State private var email = ""
-    @State private var password = ""
-    @State private var errorMessage = ""
+    @State private var showingSignup = true
     
     var body: some View {
         NavigationStack {
             VStack {
-                if isLoggingIn {
+                if viewModel.isLoggingIn {
                     Text("Logging in...")
                 } else {
-                    VStack {
-                        TextField("Email", text: $email)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .autocapitalization(.none)
-                            .padding()
-                        
-                        SecureField("Password", text: $password)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding()
-                        
-                        if !errorMessage.isEmpty {
-                            Text(errorMessage)
-                                .foregroundColor(.red)
-                        }
-                        
-                        Button("Login") {
-                            Task {
-                                await login()
-                            }
-                        }
-                        .padding()
-                    }
-                    .padding()
-                    
-                    Button("Sign Up") {
-                        showingSignup = true
-                    }
+                    loginForm
+                    signupButton
                 }
             }
             .navigationDestination(isPresented: $showingSignup) {
-                SignupView() // Make sure you have this view defined
+                SignupView() // Define this view
+            }
+            .onAppear {
+                viewModel.onDismiss = {
+                    username = viewModel.username
+                    level = viewModel.level
+                    dismiss()
+                }
             }
         }
     }
     
-    private func login() async {
-        isLoggingIn = true
-        defer { isLoggingIn = false } // This ensures that isLoggingIn is set to false when the function exits
-        
-        do {
-            let user = try await realmApp.login(credentials: .emailPassword(email: email, password: password))
-            await fetchAndSetUsername(userId: user.id)
-            DispatchQueue.main.async { // Switch to the main thread to update the UI
-                dismiss()
-            }
-        } catch {
-            DispatchQueue.main.async {
-                self.errorMessage = "Login failed: \(error.localizedDescription)"
-            }
-        }
-    }
+    private var loginForm: some View {
+        VStack {
+            TextField("Email", text: $viewModel.email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .padding()
+            
+            SecureField("Password", text: $viewModel.password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
 
-    private func fetchAndSetUsername(userId: String) async {
-        let user = realmApp.currentUser!
-        let mongoClient = user.mongoClient("mongodb-atlas")
-        let database = mongoClient.database(named: "10000H")
-        let collection = database.collection(withName: "users")
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage)
+                    .foregroundColor(.red)
+            }
 
-        do {
-            let result = try await collection.findOneDocument(filter: ["_id": AnyBSON(userId)])
-            if let result = result, let username = result["username"]??.stringValue, let level = result["level"]??.stringValue{
-                DispatchQueue.main.async {
-                    self.username = username
-                    self.level = level
+            Button("Login") {
+                Task {
+                    await viewModel.login()
                 }
             }
-        } catch {
-            DispatchQueue.main.async {
-                self.errorMessage = "Failed to fetch user data: \(error.localizedDescription)"
-            }
+            .padding()
         }
+        .padding()
     }
-
+    
+    private var signupButton: some View {
+        Button("Sign Up") {
+            showingSignup = true
+        }
+        .padding()
+    }
 }
-
-
--- frontend/ten_thousand_hours/Views/SignupView.swift
+++ frontend/ten_thousand_hours/Views/SignupView.swift
@@ -9,97 +9,35 @@ import SwiftUI
 import RealmSwift
 
 struct SignupView: View {
-    @State private var username = ""
-    @State private var email = ""
-    @State private var password = ""
+    @StateObject private var viewModel = SignupViewModel() // Instantiate ViewModel
     @State private var errorMessage = ""
 
     var body: some View {
         VStack {
-            TextField("Username", text: $username)
+            TextField("Username", text: $viewModel.username) // Bind to ViewModel
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
-            TextField("Email", text: $email)
+            TextField("Email", text: $viewModel.email) // Bind to ViewModel
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
 
-            SecureField("Password", text: $password)
+            SecureField("Password", text: $viewModel.password) // Bind to ViewModel
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
 
-            if !errorMessage.isEmpty {
-                Text(errorMessage)
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage) // Use ViewModel's errorMessage
                     .foregroundColor(.red)
             }
 
             Button("Sign Up") {
-                signup()
+                Task {
+                    viewModel.signup() // Call signup on ViewModel
+                }
             }
             .padding()
         }
         .padding()
-    }
-
-    private func signup() {
-        realmApp.emailPasswordAuth.registerUser(email: email, password: password) { [self] (error) in
-            DispatchQueue.main.async {
-                if let error = error {
-                    self.errorMessage = "Signup failed: \(error.localizedDescription)"
-                } else {
-                    // User successfully registered, now log them in
-                    loginAfterSignup(email: email, password: password)
-                    self.errorMessage = "Signup successful"
-                }
-            }
-        }
-    }
-
-    private func loginAfterSignup(email: String, password: String) {
-        let credentials = Credentials.emailPassword(email: email, password: password)
-        realmApp.login(credentials: credentials) { [self] (result) in
-            DispatchQueue.main.async {
-                switch result {
-                case .failure(let error):
-                    self.errorMessage = "Login after signup failed: \(error.localizedDescription)"
-                case .success(let user):
-                    // User is now logged in, can store additional data
-                    self.errorMessage = "Login after signup succeeded"
-                    self.storeUserData(userId: user.id, username: self.username, level: "BeginnerCat")
-                }
-            }
-        }
-    }
-
-    
-    func storeUserData(userId: String, username: String, level: String) {
-        let user = realmApp.currentUser!
-        let mongoClient = user.mongoClient("mongodb-atlas")
-        let database = mongoClient.database(named: "10000H")
-        let collection = database.collection(withName: "users")
-
-        let updateData: [String: AnyBSON] = [
-            "username": .string(username),
-            "level": .string(level)
-        ]
-
-        collection.updateOneDocument(
-            filter: ["_id": AnyBSON(stringLiteral: userId)],
-            update: ["$set": AnyBSON(updateData)],
-            upsert: true
-        ) { result in
-            switch result {
-            case .failure(let error):
-                print("Failed to update user data: \(error.localizedDescription)")
-            case .success(_):
-                self.errorMessage = "Store successful"
-            }
-        }
-    }
-}
-
-struct SignupView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignupView()
     }
 }
